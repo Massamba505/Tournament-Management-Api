0a9b1b38f1774310906c48ea8a764a08f5de1814
Massamba505 <sabelomaphalala131@gmail.com>
2025-05-21 13:30:20 +0200

    Model Validation with Attributes

--- Models/Dto/User/LoginUserDto.cs
+++ Models/Dto/User/LoginUserDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tournament.Management.API.Models.Dto.User
 {
     public class LoginUserDto
     {
+        [Required]
         public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(255)]
         public string Password { get; set; } = null!;
     }
 }
--- Models/Dto/User/RegisterUserDto.cs
+++ Models/Dto/User/RegisterUserDto.cs
@@ -1,11 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tournament.Management.API.Models.Dto.User
 {
     public class RegisterUserDto
     {
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(50)]
         public string Surname { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(100)]
         public string Email { get; set; } = null!;
+
+        [Required]
+        [StringLength(255, MinimumLength = 6)]
         public string Password { get; set; } = null!;
-        public int RoleId { get; set; }
+
+        [Required]
+        public int? RoleId { get; set; }
     }
 }
